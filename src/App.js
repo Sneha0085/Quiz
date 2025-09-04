3d7631e5633843b722356db7983cbc97df07bc02
snehanaskar <snehanaskar17112005@gmail.com>
2025-09-04 19:12:08 +0530

    Added React project files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Todo from "./pages/Todo";
+import Pro from "./pages/Pro";
+import { Calculator } from "./pages/Calculator";
+import { Quiz } from "./pages/Quiz";
+function App() {
+  return (
+    <div className="App">
+   
+     <Calculator/>
+    </div>
+  );
+}
+
+export default App;
